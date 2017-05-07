bc06c1f8ce22afbff16fe52836635c8b5c561a5b
xTACTIXzZ <luxfuxnux@gmail.com>
2017-05-07 20:05:32 +0200

    Added test module & minor fixes

---
+++ test/module/src/com/github/testmodule/TestModule.java
@@ -0,0 +1,48 @@
+package com.github.testmodule;
+
+import java.util.Date;
+
+import com.github.hydrazine.Hydrazine;
+import com.github.hydrazine.module.Module;
+
+public class TestModule implements Module
+{
+	
+	public static void main(String[] args)
+	{
+		// This code does not get executed!
+	}
+	
+	@Override
+	public String getDescription()
+	{
+		return "This is a test module, it outputs some test messages.";
+	}
+	
+	@Override
+	public String getName() 
+	{
+		return "testmodule";
+	}
+
+	@Override
+	public void start() 
+	{
+		System.out.println(Hydrazine.infoPrefix + "Module started at " + new Date().toString());
+		
+		stop();
+	}
+
+	@Override
+	public void stop() 
+	{
+		System.out.println(Hydrazine.infoPrefix + "Module stopped at " + new Date().toString());
+	}
+
+	@Override
+	public void configure() 
+	{
+		System.out.println("Nothing to configure here.");
+	}
+
+}
